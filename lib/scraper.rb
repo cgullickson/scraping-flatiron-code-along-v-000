c09f715fa60d469094ac79b9dd4e27910ea4e90d
Chris Gullickson <cgullickson5@gmail.com>
2017-12-11 20:04:31 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,9 +17,9 @@ class Scraper
             course.schedule = post.css(".date").text
             course.description = post.css("p").text
 
-            binding.pry 
+          
           end
-
+          binding.pry 
     end
 
 
